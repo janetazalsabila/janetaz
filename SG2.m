38c0c49fade42b1daf8e2715918528d7823fffe5
Janeta Zalsabila <57222066+janetazalsabila@users.noreply.github.com>
2019-11-06 22:19:06 +0700

    Add files via upload

---
+++ SG2.m
@@ -0,0 +1,15 @@
+%% PERCABANGAN
+angka = input('masukkan angka:');
+
+if angka==1
+    hasil = 'dua';
+elseif angka==8
+    hasil = 'sepuluh';
+else
+    hasil = 'bukan dua atau sepuluh';
+end
+
+disp(hasil);
+
+%% CASE
+
