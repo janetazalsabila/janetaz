05cfd50bf0068706193130a93d2929e977a20cde
Janeta Zalsabila <57222066+janetazalsabila@users.noreply.github.com>
2019-11-06 22:20:12 +0700

    Add files via upload

---
+++ SG2_5.m
@@ -0,0 +1,16 @@
+a = 1;
+while(a<=10) %kondisi
+    disp(a); %eksekusi
+    a=a+1;
+    break;
+end
+
+i=true;
+x=1;
+while i == true
+    disp(x);
+    if x==5;
+        break;
+    end
+    x=x+1;
+end
